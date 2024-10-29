5d065a6144c83de7c1c742801df191182dc1396a
Eduardo Rezende <dudumoises2005@gmail.com>
2024-10-29 14:55:46 -0300

    Improving documentation of Expression[TDelegate].Extensions class
    Updating the summary, typeparam definition and parameter name

--- src/Nina.SourceCode/Expression[TDelegate].Extensions.cs
+++ src/Nina.SourceCode/Expression[TDelegate].Extensions.cs
@@ -8,21 +8,21 @@ namespace Nina.SourceCode
     internal static class Expression_TDelegate_Extensions
     {
         /// <summary>
-        /// Gets the name of the <typeparamref name="TProperty"/> by <see cref="Expression"/>.
+        /// Gets a property name of <typeparamref name="T"/> using <paramref name="expression"/>.
         /// </summary>
         /// <typeparam name="T">Any type with default constructor.</typeparam>
-        /// <typeparam name="TProperty">The defined property.</typeparam>
-        /// <param name="property">The property.</param>
-        /// <returns>The <typeparamref name="TProperty"/> name.</returns>
+        /// <typeparam name="TProperty">The type of the property to be set.</typeparam>
+        /// <param name="expression">The expression containing the property.</param>
+        /// <returns>The property name of type <typeparamref name="TProperty"/>.</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidCastException"></exception>
-        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, TProperty>> property)
+        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, TProperty>> expression)
             where T : new()
         {            
-            if (property is null)
-                throw new ArgumentNullException(nameof(property), "The expression must not be null.");
+            if (expression is null)
+                throw new ArgumentNullException(nameof(expression), "The expression must not be null.");
 
-            var memberExpression = property.Body as MemberExpression;
+            var memberExpression = expression.Body as MemberExpression;
 
             if (memberExpression is null) 
                 throw new InvalidCastException($"Invalid expression for {typeof(T).Name}.");
@@ -31,21 +31,21 @@ namespace Nina.SourceCode
         }
 
         /// <summary>
-        /// Gets the name of the <typeparamref name="TProperty"/> by <see cref="Expression"/>.
+        /// Gets a property name of <typeparamref name="T"/> using <paramref name="expression"/>.
         /// </summary>
         /// <typeparam name="T">Any type with default constructor.</typeparam>
-        /// <typeparam name="TProperty">The defined property.</typeparam>
-        /// <param name="property">The property.</param>
-        /// <returns>The <typeparamref name="TProperty"/> name.</returns>
+        /// <typeparam name="TProperty">The type of the property to be set.</typeparam>
+        /// <param name="expression">The expression containing the property.</param>
+        /// <returns>The property name of type ICollection&lt;<typeparamref name="TProperty"/>&gt;.</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidCastException"></exception>
-        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, ICollection<TProperty>>> property)
+        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, ICollection<TProperty>>> expression)
             where T : new()
         {
-            if (property is null)
-                throw new ArgumentNullException(nameof(property), "The expression must not be null.");
+            if (expression is null)
+                throw new ArgumentNullException(nameof(expression), "The expression must not be null.");
 
-            var memberExpression = property.Body as MemberExpression;
+            var memberExpression = expression.Body as MemberExpression;
 
             if (memberExpression is null)
                 throw new InvalidCastException($"Invalid expression for {typeof(T).Name}.");
@@ -54,21 +54,21 @@ namespace Nina.SourceCode
         }
 
         /// <summary>
-        /// Gets the name of the <typeparamref name="TProperty"/> by <see cref="Expression"/>.
+        /// Gets a property name of <typeparamref name="T"/> using <paramref name="expression"/>.
         /// </summary>
         /// <typeparam name="T">Any type with default constructor.</typeparam>
-        /// <typeparam name="TProperty">The defined property.</typeparam>
-        /// <param name="property">The property.</param>
-        /// <returns>The <typeparamref name="TProperty"/> name.</returns>
+        /// <typeparam name="TProperty">The type of the property to be set.</typeparam>
+        /// <param name="expression">The expression containing the property.</param>
+        /// <returns>The property name of type ICollection&lt;ICollection&lt;<typeparamref name="TProperty"/>&gt;&gt;.</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidCastException"></exception>
-        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, ICollection<ICollection<TProperty>>>> property)
+        internal static string GetPropertyName<T, TProperty>(this Expression<Func<T, ICollection<ICollection<TProperty>>>> expression)
             where T : new()
         {
-            if (property is null)
-                throw new ArgumentNullException(nameof(property), "The expression must not be null.");
+            if (expression is null)
+                throw new ArgumentNullException(nameof(expression), "The expression must not be null.");
 
-            var memberExpression = property.Body as MemberExpression;
+            var memberExpression = expression.Body as MemberExpression;
 
             if (memberExpression is null)
                 throw new InvalidCastException($"Invalid expression for {typeof(T).Name}.");
